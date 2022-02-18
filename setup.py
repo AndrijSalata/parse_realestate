379959f29a14fc365d8d8819a00313f7a925501f
AndrijSalata <4Andrij@gmail.com>
2022-02-18 19:43:19 +0100

    console script

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'pandas',
         'tqdm',
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'parse_realestate = parse_realestate:main',
         ],
